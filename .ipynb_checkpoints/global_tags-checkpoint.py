a351595f5a1fcfd7ba272ebabec9b95ed8e85c95
Martin Kada <martin.kada@tu-berlin.de>
2022-10-20 12:30:56 +0200

    Cleanup

--- .ipynb_checkpoints/global_tags-checkpoint.py
+++
@@ -1,9 +0,0 @@
-
-class GlobalTags:
-    LEGACY_LAYER_BASE = False
-
-    def legacy_layer_base(value=None):
-        if value is not None:
-            GlobalTags.LEGACY_LAYER_BASE = value
-        return GlobalTags.LEGACY_LAYER_BASE
-    
